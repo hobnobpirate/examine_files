2299fc3f57e1e123b469d11337e18ef3d0591014
hnp <hobnobpirate@gmail.com>
2019-06-14 22:10:40 -0400

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     include_package_data=True,
     keywords="examine_files",
     name="examine_files",
-    packages=find_packages(include=["examine_files"]),
+    packages=find_packages(include=["examine_files", "file_types"]),
     setup_requires=setup_requirements,
     test_suite="tests",
     tests_require=test_requirements,
